a6e39a1acc47c1d46c0bbbe8b40e5cce3d6196cc
Satyajit Mathan <mathansatyajit@gmail.com>
2026-06-16 20:43:42 +0530

    Completed RecipeHub Project

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import App from './App.jsx'
 import './index.css'
 import { HashRouter } from "react-router-dom";
 import { ToastContainer } from 'react-toastify'
-import Recipe from './pages/Recipe.jsx'
 import RecipeContext from './context/RecipeContext.jsx'
  
 
